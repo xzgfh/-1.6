851e9242327518042d7120640d7a8ec8280df1c6
User <User@WIN-Q06VGDO4GD9>
2021-12-20 19:45:34 +0300

    Добавьте файлы проекта.

---
+++ Лаба №1.6/LinearEquation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба__1._6
+{
+    public class LinearEquation
+    {
+        private double a;
+        private double b;
+        private double c;
+        public double F()
+        {
+            if (A != 0)
+                return (C - B) / A;
+            else return double.NaN;
+        }
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+        public double C { get => c; set => c = value; }
+    }
+}
---
+++ Лаба №1.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба__1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearEquation obj = new LinearEquation()
+            {
+                A = 1,
+                B = 4,
+                C = 0
+            };
+            double result = obj.F();
+            if (double.IsNaN(result))
+            {
+                Console.WriteLine($"ошибка");
+            }
+            else
+            {
+                Console.WriteLine($"x={result}");
+            }
+        }
+    }
+}
